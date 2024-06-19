128cc2c984b6b81c8a953a285a23120c7ca142c8
unknown <vstroganov.reon@gmail.com>
2024-06-19 16:20:46 +0300

    refactoring

--- src/share/components/Nav/index.tsx
+++ src/share/components/Nav/index.tsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import styles from './index.module.scss';
-import NavItem from '../NavItem';
-import { ROUTES } from '../../consts';
+import React from "react";
+import styles from "./index.module.scss";
+import NavItem from "../NavItem";
+import { ROUTES } from "../../consts";
 
 const Nav = () => {
   return (
     <nav className={styles.nav}>
       <ul className={styles["nav__links-list"]}>
-        <NavItem path={ROUTES.main} text='Главная'></NavItem>
-        <NavItem path={ROUTES.projects}  text='Проекты'></NavItem>
-        <NavItem path={ROUTES.contacts}  text='Контакты'></NavItem>
+        <NavItem path={"/cv"} text="Главная"></NavItem>
+        <NavItem path={ROUTES.projects} text="Проекты"></NavItem>
+        <NavItem path={ROUTES.contacts} text="Контакты"></NavItem>
       </ul>
     </nav>
   );
--- src/share/components/NavItem/index.tsx
+++ src/share/components/NavItem/index.tsx
@@ -1,11 +1,11 @@
-import styles from './index.module.scss';
-import { NavLink } from 'react-router-dom';
+import styles from "./index.module.scss";
+import { NavLink } from "react-router-dom";
 
 const NavItem = (props) => {
   return (
     <NavLink
       to={props.path}
-      end 
+      end
       className={({ isActive, isPending }) =>
         isActive
           ? `${styles["links-list__item--active"]} ${styles["links-list__item"]}`
