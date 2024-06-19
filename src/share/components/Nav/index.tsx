a2018e7c70115c106600e8867fa2c7c47abac430
unknown <vstroganov.reon@gmail.com>
2024-06-19 16:23:13 +0300

    refactoring

--- src/share/components/Nav/index.tsx
+++ src/share/components/Nav/index.tsx
@@ -7,7 +7,7 @@ const Nav = () => {
   return (
     <nav className={styles.nav}>
       <ul className={styles["nav__links-list"]}>
-        <NavItem path={"/cv"} text="Главная"></NavItem>
+        <NavItem path={"/cv/"} text="Главная"></NavItem>
         <NavItem path={ROUTES.projects} text="Проекты"></NavItem>
         <NavItem path={ROUTES.contacts} text="Контакты"></NavItem>
       </ul>
